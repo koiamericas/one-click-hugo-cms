b2f5118d8713f2962d4d384931a15046d141a431
Juan Bustamante <juankmilobh@hotmail.com>
2017-10-05 16:07:56 -0500

    Izquierdo message box v0.1

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,17 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+var btn = document.getElementById("show-more-btn");
+
+var message = document.getElementById("izquiero-message");
+
+btn.addEventListener("click", function() {
+  if (message.classList.contains("show-less")){
+    message.classList.remove("show-less");
+    btn.innerHTML = "Show less" + "<img src='/img/icons/down-white-arrow.svg' class='down-white-arrow show-mobile-only rotate'/>";
+  } else {
+    message.classList.add("show-less");
+    btn.innerHTML = "Keep reading" + "<img src='/img/icons/down-white-arrow.svg' class='down-white-arrow show-mobile-only'/>";
+  }
+});
